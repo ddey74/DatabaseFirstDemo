b6328f7f5e6a67b0ca433c6815df18f4af69ce69
ddey74 <86369082+ddey74@users.noreply.github.com>
2021-06-25 09:11:01 +0530

    First Commit
    Installed EF
    Added ADO.Net Entity data model
    Added Connecttion to the SQL server
    Added Entity Data model(edmx) file
    Added Post object and using DbSet object pushed the properties of post in Db

---
+++ DataBaseFirstDemo/DataBaseFirstDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataBaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DatabaseFirstDemoEntities();
+            var post = new Post()
+            {
+                Body = "Body of the post which we want to publish",
+                DatePublished = DateTime.Now,
+                Title="My First Post",
+                PostID=1
+                
+            };
+            context.Posts.Add(post);//adding post object to the DbSet object
+            context.SaveChanges();//Will commit changes to the Database
+        }
+    }
+}
